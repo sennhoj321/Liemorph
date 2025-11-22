27a6da80a1777f0455e9413bc802efcfa1a589a0
Johannes Bostelmann <johannes.bostelmann@tuhh.de>
2025-11-22 21:33:35 +0100

    clean up

---
+++ setup.sh
@@ -0,0 +1,4 @@
+# make sure that cuda-toolkit is installed, otherwise run this bash script in a conda enviroment to 
+- conda install ninja
+- conda install cuda-toolkit=12.6
+- pip install git+https://github.com/princeton-vl/lietorch.git 
